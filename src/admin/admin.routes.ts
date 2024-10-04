d9b1d7bb6acc28f0b1660f0b2003d42f6e6de0aa
kshetritej <tejghartikshetri@gmail.com>
2024-10-04 21:59:34 +0545

    add: course add and update route

--- src/admin/admin.routes.ts
+++ src/admin/admin.routes.ts
@@ -1,11 +1,15 @@
-import {Router} from 'express';
+import { Router } from 'express';
 const adminRouter = Router();
 const bcrypt = require('bcrypt');
-import {Admin} from '../models/admin.model';
+import { Admin } from '../models/admin.model';
 import { envConfig } from '../configs/env.config';
+import { Course } from '../models/course.model';
+import { adminAuthMiddleware } from '../middleware/auth.middleware';
 const jwt = require('jsonwebtoken');
 
-adminRouter.post('/register', async(req, res) => { 
+
+//@ts-ignore
+adminRouter.post('/register', async (req, res) => {
     const { email, password, firstName, lastName } = req.body;
     const hashPassword = bcrypt.hashSync(password, 10);
 
@@ -15,43 +19,90 @@ adminRouter.post('/register', async(req, res) => {
         email,
         password: hashPassword
     })
-    res.status(201).json({
-        "Success":"Admin created successfully",
+    return res.status(201).json({
+        "Success": "Admin created successfully",
         response
     })
 })
-adminRouter.post('/login', async(req, res) => { 
-    const {email, password} = req.body;
-    const admin = await Admin.findOne({email: email});
 
-    if(admin) {
+//@ts-ignore
+adminRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({ email: email });
+
+    if (admin) {
         //@ts-ignore
         const passwordIsCorrect = await bcrypt.compareSync(password, admin.password);
-        if(passwordIsCorrect) {
-            const token = jwt.sign({admin}, envConfig.ADMIN_JWT_SECRET);
+        if (passwordIsCorrect) {
+            //@ts-ignore
+            const token = jwt.sign({ id: admin._id }, envConfig.ADMIN_JWT_SECRET);
             res.status(200).json({
-                "Success":"Login successful",
-                "token":token,
+                "Success": "Login successful",
+                "token": token,
+            })
+        } else {
+            res.status(403).json({
+                "Error": "Invalid email or password"
             })
         }
 
-        res.status(403).json({
-            "Error":"Invalid email or password"
+    }
+    return res.status(404).json({
+        "Error": "Admin not found"
+    })
+})
+//@ts-ignore
+adminRouter.post('/course', adminAuthMiddleware, async (req, res) => {
+    //@ts-ignore
+    const adminId = req.adminId;
+    const { courseName, courseDescription, coursePrice } = req.body;
+    const response = await Course.create({
+        courseName,
+        courseDescription,
+        coursePrice,
+        courseCreatorId: adminId
+    })
+    if (!response) {
+        res.status(400).json({
+            "ERROR": "Course not created"
         })
     }
-    res.status(404).json({
-        "Error":"Admin not found"
+    return res.send({
+        "Success": "Course created successfully",
+        response
     })
 })
 
-adminRouter.post('/create-course', (req, res) => {
+
+
+
+//@ts-ignore
+adminRouter.put('/course', adminAuthMiddleware, async (req, res) => {
+    //@ts-ignore
+    const admin = req.adminId;
+    const { courseName, courseDescription, coursePrice, courseId } = req.body;
+
+    if(!courseId){
+        res.json({"ERROR": "Course Id is required"})
+    }
+    if(!admin){
+    res.json({"ERROR": "NO related course found."})
+    }
+
+    const response = await Course.findOneAndUpdate({ _id: courseId, courseCreatorId: admin }, { courseName, courseDescription, coursePrice }, { new: true });
+    if (!response) {
+        res.status(400).json({
+            "Error": "Course not updated"
+        })
+    }
+    return res.status(201).json({ "Success": "Course updated successfully" })
 
 })
 
 
-
-adminRouter.delete('/courses/:id', (req, res) => {
+//@ts-ignore
+adminRouter.delete('/courses/:id', adminAuthMiddleware, (req, res) => {
     res.send("Delete course")
 })
 
-export {adminRouter}
+export { adminRouter }
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,7 +3,7 @@ import { envConfig } from "../configs/env.config";
 
 const jwt = require('jsonwebtoken');
 
-const userAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const userAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.token;
     if (!token) {
         return res.status(401).send('Unauthorized!');
@@ -11,27 +11,29 @@ const userAuthMiddleware = (req: Request, res: Response, next: NextFunction) => 
 
     const decoded = jwt.verify(token, envConfig.USER_JWT_SECRET)
 
-    if (decoded) {
-        //@ts-ignore
-        req.userId = decoded.userId;
-        next();
+    if (!decoded) {
+        return res.status(401).send('You are not signed in!');
     }
-    return res.status(401).send('You are not signed in!');
+    //@ts-ignore
+    req.userId = decoded.userId;
+    next();
 }
 
 
 
-const adminAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const adminAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.token;
+    console.log('token:', token)
     if (!token) {
         return res.status(401).send('Unauthorized!');
     }
     const decoded_id = jwt.verify(token, envConfig.ADMIN_JWT_SECRET)
 
-    if (decoded_id) {
-        //@ts-ignore
-        req.adminId = decoded_id.adminId;
-        next();
+    if (!decoded_id) {
+        return res.status(401).send('You are not signed in!');
     }
-    return res.status(401).send('You are not signed in!');
+    //@ts-ignore
+    req.adminId = decoded_id.id;
+    next();
 }
+
