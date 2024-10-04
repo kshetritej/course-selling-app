ec7319add9371a097cc659567e6b36e125de6170
kshetritej <tejghartikshetri@gmail.com>
2024-10-04 18:43:02 +0545

    add: auth middleware for token

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,17 +1,37 @@
 import { Response, Request, NextFunction } from "express";
+import { envConfig } from "../configs/env.config";
 
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'secret';
 
-const authMiddleware = (req:Request , res:Response, next: NextFunction) => {
-    const token = req.header;
-
+const userAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.token;
     if (!token) {
         return res.status(401).send('Unauthorized!');
     }
-    const isVerified = jwt.compare(token, JWT_SECRET)
 
-    if (isVerified) {
+    const decoded = jwt.verify(token, envConfig.USER_JWT_SECRET)
+
+    if (decoded) {
+        //@ts-ignore
+        req.userId = decoded.userId;
         next();
     }
+    return res.status(401).send('You are not signed in!');
+}
+
+
+
+const adminAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.token;
+    if (!token) {
+        return res.status(401).send('Unauthorized!');
+    }
+    const decoded_id = jwt.verify(token, envConfig.ADMIN_JWT_SECRET)
+
+    if (decoded_id) {
+        //@ts-ignore
+        req.adminId = decoded_id.adminId;
+        next();
+    }
+    return res.status(401).send('You are not signed in!');
 }
